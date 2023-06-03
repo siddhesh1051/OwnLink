a7bc1ce0cc9becf4a0abc5a200aded97706e8c2e
siddhesh1051 <siddhesh1052001@gmail.com>
2023-06-04 02:43:39 +0530

    ui

--- client/src/components/Right.jsx
+++ client/src/components/Right.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Box, Tab,Tabs} from '@mui/material';
+import { Avatar, Box, Tab,Tabs, TextField} from '@mui/material';
 import { TabPanel,TabList, TabContext} from '@mui/lab';
 import "../../src/App.css"
 
@@ -14,23 +14,31 @@ const Right = () => {
     <div className='flex-1' >
      <TabContext value={value}>
   
-    <TabList onChange={handleChange} aria-label="lab API tabs example" centered >
+    <TabList onChange={handleChange} aria-label="lab API tabs example"  textColor='[white]' indicatorColor='secondary'>
  
-      <Tab label="Bio" value="1" textColor="secondary"
-  indicatorColor="secondary"/>
+      <Tab label="Bio" value="1" />
       <Tab label="Links" value="2" />
       <Tab label="Appearance" value="3" />
       
     </TabList>
   <TabPanel value="1">
-  <input className="inputBox"
-            type="password"
-            placeholder="Password"
-            name='password'
-            autoComplete='off'
-           
-            
-          />
+    <div className=' flex justify-center' >
+
+  <Avatar alt="Remy Sharp" src="https://api.multiavatar.com/kitty.svg" />
+    </div>
+    <div className=' flex justify-center gap-4 flex-col' >
+      <div  className='flex justify-start gap-4 '>
+
+  <TextField id="filled-basic" label="Username" variant="filled" placeholder='Choose a Username' fullWidth  color="secondary" sx={{ input: { color: 'white' } }} />
+  <TextField id="filled-basic" label="Name" variant="filled" placeholder='Full Name' fullWidth/>
+
+      </div>
+    <div>
+
+  <TextField id="filled-basic" label="Description" variant="filled" placeholder='Description'fullWidth/>
+    </div>
+    </div>
+ 
   </TabPanel>
   <TabPanel value="2">Item Two</TabPanel>
   <TabPanel value="3">Item Three</TabPanel>
