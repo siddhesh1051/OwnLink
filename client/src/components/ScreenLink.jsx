fbae808981db65b9bc8668b9be8753725aca9ca7
siddhesh1051 <siddhesh1052001@gmail.com>
2023-06-06 15:28:05 +0530

    screen ui .

--- client/src/components/ScreenLink.jsx
+++ client/src/components/ScreenLink.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import landImg from './img/bg1.png'
+import landImg from './img/landImg.jpeg'
 const ScreenLink = () => {
   return (
     <div className='flex text-white w-[90%] mt-2 min-w-[80%] min-h-[150px] rounded-xl '>
-      <img src={landImg} alt="" className='' />
+      <img src={landImg} alt="" className=' object-cover h-full w-full' />
     </div>
   )
 }
