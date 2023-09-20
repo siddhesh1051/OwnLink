95088ab46071011cb290b465d855b80e98c5b435
siddhesh1051 <siddhesh1052001@gmail.com>
2023-09-21 01:21:06 +0530

    Landing Page Ui

---
+++ client/src/components/LandingPage/components/Features.jsx
@@ -0,0 +1,142 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <div>
+     <div className="py-16">
+  <div className="xl:container m-auto space-y-12 px-6 md:px-12 lg:px-20">
+    <div>
+      <h2 className="mt-4 text-center text-2xl font-bold text-gray-800 dark:text-white md:text-4xl">
+        A technology-first approach to payments{" "}
+        <br className="sm:block" hidden="" />
+        and finance
+      </h2>
+    </div>
+    <div className="mt-16 grid gap-8 sm:w-2/3 sm:mx-auto md:w-full md:grid-cols-2 md:-mx-8 lg:grid-cols-3">
+      <div className="p-8 py-12 sm:p-12 rounded-3xl bg-white border border-gray-100 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
+        <div className="space-y-12 text-center">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/4341/4341069.png"
+            className="w-16 mx-auto"
+            width={512}
+            height={512}
+            alt="burger illustration"
+          />
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold text-gray-800 transition dark:text-white">
+              First feature
+            </h3>
+            <p className="text-gray-600 dark:text-gray-300">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+              nesciunt neque culpa hic illum ab qui error repellendus asperiores
+              unde ut ipsam perferendis nemo fuga cum, eum consectetur, magnam
+              doloremque!
+            </p>
+            <a
+              href="#"
+              className="group relative flex mx-auto h-12 w-12 items-center justify-center before:absolute before:inset-0 before:rounded-full before:border before:border-gray-200 before:bg-gray-50 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="relative w-5 h-5 text-gray-600 dark:text-white transition duration-300 group-hover:translate-x-1"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="p-8 py-12 sm:p-12 rounded-3xl bg-white border border-gray-100 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
+        <div className="space-y-12 text-center">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/4341/4341152.png"
+            className="w-16 mx-auto"
+            width={512}
+            height={512}
+            alt="burger illustration"
+          />
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold text-gray-800 transition dark:text-white">
+              Second feature
+            </h3>
+            <p className="text-gray-600 dark:text-gray-300">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+              nesciunt neque culpa hic illum ab qui error repellendus asperiores
+              unde ut ipsam perferendis nemo fuga cum, eum consectetur, magnam
+              doloremque!
+            </p>
+            <a
+              href="#"
+              className="group relative flex mx-auto h-12 w-12 items-center justify-center before:absolute before:inset-0 before:rounded-full before:border before:border-gray-200 before:bg-gray-50 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="relative w-5 h-5 text-gray-600 dark:text-white transition duration-300 group-hover:translate-x-1"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="p-8 py-12 sm:p-12 rounded-3xl bg-white border border-gray-100 dark:shadow-none dark:border-gray-700 dark:bg-gray-800 bg-opacity-50 shadow-2xl shadow-gray-600/10">
+        <div className="space-y-12 text-center">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/4341/4341146.png"
+            className="w-16 mx-auto"
+            width={512}
+            height={512}
+            alt="burger illustration"
+          />
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold text-gray-800 transition dark:text-white">
+              Third feature
+            </h3>
+            <p className="text-gray-600 dark:text-gray-300">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe
+              nesciunt neque culpa hic illum ab qui error repellendus asperiores
+              unde ut ipsam perferendis nemo fuga cum, eum consectetur, magnam
+              doloremque!
+            </p>
+            <a
+              href="#"
+              className="group relative flex mx-auto h-12 w-12 items-center justify-center before:absolute before:inset-0 before:rounded-full before:border before:border-gray-200 before:bg-gray-50 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="relative w-5 h-5 text-gray-600 dark:text-white transition duration-300 group-hover:translate-x-1"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+    </div>
+  )
+}
+
+export default Features
