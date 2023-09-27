370775604ecbab7084bc3e110d98172dd07a446c
siddhesh1051 <siddhesh1052001@gmail.com>
2023-09-28 00:26:00 +0530

    Added Profile Menu

---
+++ client/src/components/LogoutMenu.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react'
+import { Avatar, IconButton, Menu, MenuItem } from "@mui/material";
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast'
+import { LuLogOut } from 'react-icons/lu';
+
+
+
+
+const LogoutMenu = ({ update, handleQrOpen }) => {
+
+  const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(null);
+  let email = localStorage.getItem('email');
+  let username = localStorage.getItem("username")
+  // const [profilePic, setProfilePic] = useState("");
+
+
+  // const getProfilePic = async () => {
+  //   try{
+  //     const {data} = await axios.get(`${process.env.REACT_APP_API}/profilepic/${email}`)
+  //     setProfilePic(data.profilePic);
+
+  //   }
+  //   catch(err){
+  //     console.log(err)
+  //   }
+  // }
+
+  useEffect(() => {
+    username = localStorage.getItem("username")
+    email = localStorage.getItem("email")
+
+
+  }, [update])
+
+
+
+
+  const logOut = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("email");
+    localStorage.removeItem("username");
+    localStorage.removeItem("userData");
+    toast.success("Logged out Successfully");
+    navigate("/routes/auth");
+  };
+
+  const handleClick = (event) => {
+    setMenuOpen(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setMenuOpen(null);
+  };
+
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", boxShadow: 'none' }} >
+      <IconButton onClick={handleClick} sx={{
+        backgroundColor: '#222430',
+        transition: '0.3s',
+        border: '1px solid #3A3D4F',
+        marginTop: '7px',
+        marginLeft: '7px',
+        '&:hover': {
+          backgroundColor: '#3A3D4F',
+        },
+
+
+      }} >
+        <Avatar alt="Placeholder"
+          style={{ cursor: "pointer" }} sx={{ width: 40, height: 40 }} />
+      </IconButton>
+      <Menu
+        anchorEl={menuOpen}
+        open={Boolean(menuOpen)}
+        onClose={handleClose}
+        variant='plain'
+      >
+        <MenuItem onClick={() =>
+          toast.success(`Your Email is ${email}`, {
+            duration: 5000,
+            position: 'top-right',
+            icon: '📧',
+            style: {
+              backgroundColor: 'white',
+              color: 'black',
+              border: '1px solid #3A3D4F',
+
+            },
+
+          })
+        } sx={{ fontSize: '16px' }}>My Email</MenuItem>
+        <MenuItem onClick={() => {
+            username !== null ? handleQrOpen() : toast.error("Please Claim your username first")
+          }
+        } sx={{ fontSize: '16px' }}>My QR</MenuItem>
+        <MenuItem onClick={logOut} sx={{ fontSize: '16px', color: 'red' }}>Logout <LuLogOut className='inline lg:text-md text-sm ml-2 text-red-500' /></MenuItem>
+      </Menu>
+    </div>
+  )
+}
+
+export default LogoutMenu
--- client/src/store/picSlice.js
+++ client/src/store/picSlice.js
@@ -2,61 +2,55 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import axios from 'axios';
 
 const initialState = {
-   pic: "",
+  pic: "",
 }
 
 export const getPic = createAsyncThunk(
-    "ownlink/getPic",
-    async (email) => {
-      const {
-        data: { pic },
-      } = await axios.get(process.env.REACT_APP_API +`/profilepic/${email}`);
-      console.log(pic);
-      return pic;
-    }
-  );
+  "ownlink/getPic",
+  async (email) => {
+    const {data: { profilePic }} = await axios.get(process.env.REACT_APP_API + `/profilepic/${email}`);
+    console.log(profilePic)
+    return profilePic;
+  }
+);
 
 export const getPicFromUsername = createAsyncThunk(
-    "ownlink/getPicFromUsername",
-    async (username) => {
-      const {
-        data: { pic },
-        
-      } = await axios.get(process.env.REACT_APP_API +`/profilepicfromusername/${username}`);
-      // console.log(bio)
-      return pic;
-    }
-  );
+  "ownlink/getPicFromUsername",
+  async (username) => {
+    const {data: { pic }} = await axios.get(process.env.REACT_APP_API + `/profilepicfromusername/${username}`);
+    return pic;
+  }
+);
 
- 
+
 
 const picSlice = createSlice({
-    name: 'pic',
-    initialState,
-    reducers: { 
-        add(state, action) {
-            state.push(action.payload);
+  name: 'pic',
+  initialState,
+  reducers: {
+    add(state, action) {
+      state.push(action.payload);
 
-        },
-        remove(state, action) {
-            state = state.filter((item) => item.id !== action.payload)
-        },
-        },
-    
-        extraReducers: (builder) => {
-      
-            builder.addCase(getPic.fulfilled, (state, action) => {
-              state.pic = action.payload;
-            });
-            builder.addCase(getPicFromUsername.fulfilled, (state, action) => {
-              state.pic = action.payload;
-            });
-        }
-    })
+    },
+    remove(state, action) {
+      state = state.filter((item) => item.id !== action.payload)
+    },
+  },
+
+  extraReducers: (builder) => {
+
+    builder.addCase(getPic.fulfilled, (state, action) => {
+      state.pic = action.payload;
+    });
+    builder.addCase(getPicFromUsername.fulfilled, (state, action) => {
+      state.pic = action.payload;
+    });
+  }
+})
 
 
-    export const { add, remove } = picSlice.actions
-    export default picSlice.reducer
+export const { add, remove } = picSlice.actions
+export default picSlice.reducer
 
-           
+
 
