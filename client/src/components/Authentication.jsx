2e64788a234b798c9156979d0c7d6a4d31ed4920
siddhesh-skille <siddesh.patil@skille.ai>
2024-03-16 23:36:38 +0530

    removed login anumations

--- client/src/components/Authentication.jsx
+++ client/src/components/Authentication.jsx
@@ -1,51 +1,41 @@
-import React, { useState } from 'react'
-import SignupImage from './img/SignupImage.svg'
-import LoginImage from './img/LoginImage.svg'
-import "../../src/App.css"
-import 'react-toastify/dist/ReactToastify.css'
-import Login from './Login';
-import Register from './Register';
-import { motion } from 'framer-motion';
-
+import React, { useState } from "react";
+import SignupImage from "./img/SignupImage.svg";
+import LoginImage from "./img/LoginImage.svg";
+import "../../src/App.css";
+import "react-toastify/dist/ReactToastify.css";
+import Login from "./Login";
+import Register from "./Register";
+import { motion } from "framer-motion";
 
 const Authentication = () => {
-
-  const [isNew, setisNew] = useState(false)
+  const [isNew, setisNew] = useState(false);
 
   return (
-    <div className='flex bg-gradient-to-br from-[#d6d2db] to-[#c2bccb] min-h-[100vh] w-full'>
-
-
-      <div className='container bg-white flex justify-center items-center h-full  mx-[8%] my-10 rounded-xl lg:rounded-none p-1 lg:p-0'  >
-        {
-          isNew ? <Register isNew={isNew} setisNew={setisNew} />
-
-            : <Login isNew={isNew} setisNew={setisNew} />
-        }
+    <div className="flex bg-gradient-to-br from-[#d6d2db] to-[#c2bccb] min-h-[100vh] w-full">
+      <div className="container bg-white flex justify-center items-center h-full  mx-[8%] my-10 rounded-xl lg:rounded-none p-1 lg:p-0">
+        {isNew ? (
+          <Register isNew={isNew} setisNew={setisNew} />
+        ) : (
+          <Login isNew={isNew} setisNew={setisNew} />
+        )}
         <motion.div
-          initial={{ scale: 0, opacity: 0 }}
+          initial={{ scale: 0.8, opacity: 0 }}
           animate={{ scale: 1, opacity: 1 }}
           transition={{
-            delay: 0.2,
-            duration: 0.4
+            delay: 0.1,
+            duration: 0.4,
           }}
-          className=' modal-right lg:flex-[1.3] lg:overflow-y-hidden lg:flex hidden '>
-          {
-            isNew ?
-              <img className='hero' src={SignupImage} alt="" />
-
-              :
-              <img className='hero' src={LoginImage} alt="" />
-          }
-
+          className=" modal-right lg:flex-[1.3] lg:overflow-y-hidden lg:flex hidden "
+        >
+          {isNew ? (
+            <img className="hero" src={SignupImage} alt="" />
+          ) : (
+            <img className="hero" src={LoginImage} alt="" />
+          )}
         </motion.div>
-
       </div>
-
-
     </div>
+  );
+};
 
-  )
-}
-
-export default Authentication
+export default Authentication;
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,34 +1,32 @@
-import React, { useState } from 'react'
-import "../../src/App.css"
+import React, { useState } from "react";
+import "../../src/App.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-import Sparkle from './img/sparkle.png'
-import { useFormik } from 'formik'
-import { validationSchema } from '../schema/index.jsx'
-import toast from 'react-hot-toast'
-import { motion } from 'framer-motion';
-import Spinner from './Spinner';
+import Sparkle from "./img/sparkle.png";
+import { useFormik } from "formik";
+import { validationSchema } from "../schema/index.jsx";
+import toast from "react-hot-toast";
+import { motion } from "framer-motion";
+import Spinner from "./Spinner";
 
 const Register = ({ isNew, setisNew }) => {
   const navigate = useNavigate();
   const [isLoading, setisLoading] = useState(false);
 
-
-
   const initialvalues = {
-    email: '',
-    password: '',
-  }
-  const generateError = (error) =>{
+    email: "",
+    password: "",
+  };
+  const generateError = (error) => {
     toast.error(error, {
       position: "bottom-right",
     });
-  }
+  };
 
   const onRegister = async (values, action) => {
     // console.log('Form data register', values);
-    setisLoading(true)
+    setisLoading(true);
 
     try {
       const { data } = await axios.post(
@@ -43,7 +41,7 @@ const Register = ({ isNew, setisNew }) => {
       const token = data.token;
       const user = data.user;
 
-      localStorage.setItem('token', token);
+      localStorage.setItem("token", token);
       console.log(user);
 
       if (data) {
@@ -52,162 +50,124 @@ const Register = ({ isNew, setisNew }) => {
           if (email) generateError(email);
           else if (password) generateError(password);
         } else {
-          
-          toast.success("Registration Successfull")
-          localStorage.setItem('email',user.email)
+          toast.success("Registration Successfull");
+          localStorage.setItem("email", user.email);
           navigate("/");
           window.location.reload();
-
         }
       }
     } catch (ex) {
-      if(ex.response && ex.response.status === 400){
-        toast.error("User already registered", {
-        })
+      if (ex.response && ex.response.status === 400) {
+        toast.error("User already registered", {});
+      } else {
+        toast.error("Something went wrong", {});
       }
-      else{
-        toast.error("Something went wrong", {
-        })
-      }
-      
     }
-    setisLoading(false)
+    setisLoading(false);
 
     action.resetForm();
-  }
+  };
 
-  const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
-    initialValues: initialvalues,
-    onSubmit: onRegister,
-    validationSchema: validationSchema
-
-  })
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: initialvalues,
+      onSubmit: onRegister,
+      validationSchema: validationSchema,
+    });
   return (
     <>
-      <form onSubmit={handleSubmit} className='flex-1 flex justify-start items-start'>
-
-      <div className='modal-left flex-1 flex flex-col gap-2 justify-start lg:items-start items-center ml-2'>
-          <motion.img src={Sparkle} alt=""
-           initial={{ y:-20, opacity: 0 }}
-           animate={{ y:0, opacity: 1}}
-           transition={{
-             delay: 0.1,
-             duration: 0.3
-           }}
-           
-
-             className=' w-24 h-24 ml-6' />
-          <motion.h1 
-          initial={{ y:-20, opacity: 0 }}
-          animate={{ y:0, opacity: 1}}
-          transition={{
-            delay: 0.2,
-            duration: 0.3
-          }}
-           className=' text-3xl font-medium text-black mx-8'>Get your <span className=' text-5xl font-bold text-black'>OwnLink</span></motion.h1>
-          <motion.p 
-          initial={{ y:-20, opacity: 0 }}
-          animate={{ y:0, opacity: 1}}
-          transition={{
-            delay: 0.3,
-            duration: 0.3
-          }}
-          className=' lg:text-lg text-lg text-[#979696] mx-8'>Create an account 👋</motion.p>
-          <motion.input 
-          initial={{ y:-20, opacity: 0 }}
-          animate={{ y:0, opacity: 1}}
-          transition={{
-            delay: 0.4,
-            duration: 0.3
-          }}
-          className="inputBox m-1 lg:ml-8 lg:w-[70%] w-[90%]"
+      <motion.form
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          delay: 0.1,
+          duration: 0.3,
+        }}
+        onSubmit={handleSubmit}
+        className="flex-1 flex justify-start items-start"
+      >
+        <div className="modal-left flex-1 flex flex-col gap-2 justify-start lg:items-start items-center ml-2">
+          <img src={Sparkle} alt="" className=" w-24 h-24 ml-6" />
+          <h1 className=" text-3xl font-medium text-black mx-8">
+            Get your{" "}
+            <span className=" text-5xl font-bold text-black">OwnLink</span>
+          </h1>
+          <p className=" lg:text-lg text-lg text-[#979696] mx-8">
+            Create an account 👋
+          </p>
+          <input
+            className="inputBox m-1 lg:ml-8 lg:w-[70%] w-[90%]"
             type="email"
             placeholder="Email Address"
-            name='email'
+            name="email"
             value={values.email}
             onChange={handleChange}
             onBlur={handleBlur}
-
           />
-          {
-            errors.email && touched.email ? <div className='text-red-500 ml-10 -mt-3'>{"*" + errors.email}</div> : null
-          }
-         <motion.input 
-         initial={{ y:-20, opacity: 0 }}
-         animate={{ y:0, opacity: 1}}
-         transition={{
-           delay: 0.5,
-           duration: 0.3
-         }}
-         className="inputBox m-1 lg:ml-8 lg:w-[70%] w-[90%]"
+          {errors.email && touched.email ? (
+            <div className="text-red-500 ml-10 -mt-3">{"*" + errors.email}</div>
+          ) : null}
+          <input
+            className="inputBox m-1 lg:ml-8 lg:w-[70%] w-[90%]"
             type="password"
             placeholder="Password"
-            name='password'
-            autoComplete='off'
+            name="password"
+            autoComplete="off"
             value={values.password}
             onChange={handleChange}
             onBlur={handleBlur}
           />
-          {
-            errors.password && touched.password ? <div className='text-red-500 ml-10 -mt-3'>{"*" + errors.password}</div> : null
-          }
-         <motion.input 
-         initial={{ y:-20, opacity: 0 }}
-         animate={{ y:0, opacity: 1}}
-         transition={{
-           delay: 0.6,
-           duration: 0.3
-         }}
-         className="inputBox m-1 lg:ml-8 lg:w-[70%] w-[90%]"
+          {errors.password && touched.password ? (
+            <div className="text-red-500 ml-10 -mt-3">
+              {"*" + errors.password}
+            </div>
+          ) : null}
+          <input
+            className="inputBox m-1 lg:ml-8 lg:w-[70%] w-[90%]"
             type="password"
             placeholder="Confirm Password"
-            name='confirmPassword'
-            autoComplete='off'
+            name="confirmPassword"
+            autoComplete="off"
             value={values.confirmPassword}
             onChange={handleChange}
             onBlur={handleBlur}
           />
-          {
-            errors.confirmPassword && touched.confirmPassword ? <div className='text-red-500 ml-10 -mt-3'>{"*" + errors.confirmPassword}</div> : null
-          }
+          {errors.confirmPassword && touched.confirmPassword ? (
+            <div className="text-red-500 ml-10 -mt-3">
+              {"*" + errors.confirmPassword}
+            </div>
+          ) : null}
 
-{
-            isLoading
-              ? 
-              <motion.button 
-              initial={{  opacity: 0}}
-              animate={{ opacity: 1}}
-              transition={{
-                delay: 0.75, 
-                duration: 0.3
+          {isLoading ? (
+            <button
+              type="submit"
+              className="bg-[#6D42B9] hover:bg-[#5c369f] active:scale-95 text-white text-lg  w-[90%] lg:w-[70%] h-12 rounded-lg mx-8 mt-2 duration-300"
+            >
+              <Spinner />
+            </button>
+          ) : (
+            <button
+              type="submit"
+              className="bg-[#6D42B9] hover:bg-[#5c369f] active:scale-95 text-white text-lg  w-[90%] lg:w-[70%] h-12 rounded-lg mx-8 mt-2 duration-300"
+            >
+              Sign Up
+            </button>
+          )}
+          <p className="lg:ml-8 mt-2 lg:mt-1 mb-6 lg:mb-1">
+            Already have an account{" "}
+            <span
+              className="text-[#6D42B9] cursor-pointer ml-1 font-semibold"
+              onClick={() => {
+                setisNew(!isNew);
               }}
-              type="submit" className='bg-[#6D42B9] hover:bg-[#5c369f] active:scale-95 text-white text-lg  w-[90%] lg:w-[70%] h-12 rounded-lg mx-8 mt-2 duration-300'><Spinner/>
-              </motion.button>
-              :
-          <motion.button 
-          initial={{  opacity: 0}}
-          animate={{ opacity: 1}}
-          transition={{
-            delay: 0.75, 
-            duration: 0.3
-          }}
-          type="submit" className='bg-[#6D42B9] hover:bg-[#5c369f] active:scale-95 text-white text-lg  w-[90%] lg:w-[70%] h-12 rounded-lg mx-8 mt-2 duration-300'>Sign Up
-          </motion.button>
-}
-          <motion.p 
-          initial={{ y:-20, opacity: 0 }}
-          animate={{ y:0, opacity: 1}}
-          transition={{
-            delay: 0.85,
-            duration: 0.3
-          }}
-          className='lg:ml-8 mt-2 lg:mt-1 mb-6 lg:mb-1' >Already have an account <span className='text-[#6D42B9] cursor-pointer ml-1 font-semibold' onClick={() => { setisNew(!isNew) }}>Log in</span></motion.p>
-
+            >
+              Log in
+            </span>
+          </p>
         </div>
-
-      </form>
+      </motion.form>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
