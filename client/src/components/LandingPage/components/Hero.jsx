c3584d7badf0332d99d95d6daea4ea2b10ffedfb
siddhesh-turbostart <siddhesh.p@tsquaredc.com>
2024-12-07 22:47:16 +0530

    added a flashy introducing ownlink rewards button

--- client/src/components/LandingPage/components/Hero.jsx
+++ client/src/components/LandingPage/components/Hero.jsx
@@ -4,12 +4,11 @@ import Logo from "../img/logo-3d-2.png";
 import "../../../index.css";
 import { Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion"; // Import motion
-import { FaUser } from "react-icons/fa";
+import { FaArrowRight, FaUser } from "react-icons/fa";
 
 const Hero = () => {
   const navigate = useNavigate();
 
-  // Define the motion animation properties
   const animationProps = {
     initial: { y: 30, opacity: 0 },
     transition: { delay: 0.1, duration: 0.6 },
@@ -25,80 +24,71 @@ const Hero = () => {
   return (
     <motion.div
       {...animationProps}
-      className="md:px-6 md:p-5 rounded-xl bg-[#222430] w-full"
+      className="md:px-6 md:p-5 rounded-xl bg-[#222430] w-full "
     >
-      <header className="p-0">
+      <header className="p-0 w-full">
         <input
           type="checkbox"
           name="hbr"
           id="hbr"
-          className="hbr peer "
+          className="hbr peer"
           hidden="true"
           aria-hidden="true"
         />
-        <nav className="fixed z-20 w-full bg-black/40 text-white backdrop-blur navbar shadow-md shadow-gray-600/5 peer-checked:navbar-active md:relative md:bg-transparent ">
-          <div className="xl:container px-6 md:px-1">
-            <div className="flex flex-wrap items-center justify-between gap-6 md:py-3 md:gap-0">
-              <div className="w-full flex justify-between lg:w-auto">
+        <nav className="fixed z-20 w-full bg-black/40 text-white backdrop-blur navbar shadow-md shadow-gray-600/5 peer-checked:navbar-active md:relative md:bg-transparent flex justify-center items-center">
+          <div className="container px-4 md:px-6">
+            <div className="flex flex-wrap items-center gap-6 md:py-3 md:gap-0 justify-between">
+              {/* Logo Section */}
+              <div className="flex justify-center lg:justify-start">
                 <a
                   href="/"
                   aria-label="logo"
                   className="flex space-x-2 items-center"
                 >
                   <img src={Logo} alt="logo" className="w-12 h-12 rounded-lg" />
-                  <span className="text-xl font-boldtext-primary font-semibold tracking-wide">
+                  <span className="text-xl font-bold text-primary tracking-wide">
                     Ownlink
                   </span>
                 </a>
-                <label
-                  htmlFor="hbr"
-                  className="peer-checked:hamburger block relative z-20 p-6 cursor-pointer lg:hidden"
-                >
-                  <div
-                    aria-hidden="true"
-                    className="m-auto h-0.5 w-6 rounded bg-gray-300 transition duration-300"
-                  />
-                  <div
-                    aria-hidden="true"
-                    className="m-auto mt-2 h-0.5 w-6 rounded bg-gray-300 transition duration-300"
-                  />
-                </label>
               </div>
-              <div className="navmenu hidden w-full flex-wrap justify-end items-center mb-16 space-y-8 p-6 border  rounded-3xl shadow-2xl shadow-gray-300/20 bg-gray-800 lg:space-y-0 lg:p-0 lg:m-0 lg:flex md:flex-nowrap lg:bg-transparent lg:w-7/12 lg:shadow-none border-gray-700 lg:border-0">
-                <div className="text-gray-300 lg:pr-4">
-                  <ul className="space-y-6 tracking-wide font-medium text-base lg:text-sm lg:flex lg:space-y-0">
-                    <li>
-                      <a
-                        href="/"
-                        className=" cursor-pointer block md:px-4 transition hover:text-primaryLight"
-                      >
-                        <span>Home</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="/"
-                        className=" cursor-pointer block md:px-4 transition hover:text-primaryLight"
-                      >
-                        <span>Analytics</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="/"
-                        className=" cursor-pointer block md:px-4 transition hover:text-primaryLight "
-                      >
-                        <span>Backgrounds</span>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
+
+              {/* Hamburger Menu */}
+              <label
+                htmlFor="hbr"
+                className="peer-checked:hamburger block relative z-20 p-6 cursor-pointer lg:hidden"
+              >
+                <div
+                  aria-hidden="true"
+                  className="m-auto h-0.5 w-6 rounded bg-gray-300 transition duration-300"
+                />
+                <div
+                  aria-hidden="true"
+                  className="m-auto mt-2 h-0.5 w-6 rounded bg-gray-300 transition duration-300"
+                />
+              </label>
+
+              {/* Rewards Button */}
+              <div className="hidden lg:flex">
+                <button className="group relative grid overflow-hidden rounded-full px-4 py-1 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200">
+                  <span>
+                    <span className="spark mask-gradient absolute inset-0 h-[100%] w-[100%] animate-flip overflow-hidden rounded-full [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] before:animate-rotate before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)] before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]" />
+                  </span>
+                  <span className="backdrop absolute inset-[1px] rounded-full bg-[#1b0130] transition-colors duration-200 group-hover:bg-[#330359]" />
+                  <span className="z-10 py-0.5 text-sm text-neutral-100 flex items-center justify-center gap-1">
+                    ✨ Introducing Ownlink Rewards
+                    <FaArrowRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+                  </span>
+                </button>
+              </div>
+
+              {/* Nav Menu */}
+              <div className="navmenu hidden w-full flex-wrap justify-end items-center mb-16 space-y-8 p-6 border rounded-3xl shadow-2xl shadow-gray-300/20 bg-gray-800 lg:space-y-0 lg:p-0 lg:m-0 lg:flex md:flex-nowrap lg:bg-transparent lg:w-auto lg:shadow-none border-gray-700 lg:border-0">
                 <div className="w-full space-y-2 border-gray-700 flex flex-col -ml-1 sm:flex-row lg:space-y-0 md:w-max lg:border-l">
                   <Link
                     to="/routes/auth"
-                    className=" cursor-pointer relative flex h-9 ml-auto items-center justify-center sm:px-6 before:absolute before:inset-0 before:rounded-full focus:before:bg-primaryLight/10 before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95"
+                    className="cursor-pointer relative flex h-9 ml-auto items-center justify-center sm:px-6 before:absolute before:inset-0 before:rounded-full focus:before:bg-primaryLight/10 before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95"
                   >
-                    <span className="relative text-sm font-semibold text-primaryLight">
+                    <span className="relative text-sm font-semibold text-purple-500">
                       Sign Up
                     </span>
                   </Link>
@@ -106,7 +96,7 @@ const Hero = () => {
                     to="/routes/auth"
                     className="relative flex h-9 ml-auto items-center justify-center sm:px-6 before:absolute before:inset-0 before:rounded-full before:bg-primaryLight before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95"
                   >
-                    <span className="relative text-sm font-semibold text-gray-900">
+                    <span className="relative text-sm font-semibold text-neutral-50">
                       Login
                     </span>
                   </Link>
@@ -116,8 +106,21 @@ const Hero = () => {
           </div>
         </nav>
       </header>
-      <div className="pt-32 md:py-12 xl:container m-auto px-6 md:px-20">
-        {/* Wrap the following div in motion.div */}
+      {/* Hero Section */}
+      <div className="pt-24 md:py-12 xl:container m-auto px-6 md:px-20">
+        {/* Rewards Button */}
+        <div className="lg:hidden flex mb-8 justify-center items-center">
+          <button className="group relative grid overflow-hidden rounded-full px-4 py-1 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200">
+            <span>
+              <span className="spark mask-gradient absolute inset-0 h-[100%] w-[100%] animate-flip overflow-hidden rounded-full [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] before:animate-rotate before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)] before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]" />
+            </span>
+            <span className="backdrop absolute inset-[1px] rounded-full bg-neutral-900 transition-colors duration-200 group-hover:bg-neutral-800" />
+            <span className="z-10 py-0.5 text-sm text-neutral-100 flex items-center justify-center gap-1">
+              ✨ Introducing Ownlink Rewards
+              <FaArrowRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+            </span>
+          </button>
+        </div>
         <motion.div {...animationProps}>
           <div
             aria-hidden="true"
@@ -125,7 +128,7 @@ const Hero = () => {
           />
           <div className="relative lg:flex lg:items-center lg:gap-12">
             <div className="text-center lg:text-left md:mt-12 lg:mt-0 sm:w-10/12 md:w-2/3 sm:mx-auto lg:mr-auto lg:w-6/12">
-              <h1 className=" font-bold text-4xl md:text-6xl lg:text-5xl xl:text-6xl text-white">
+              <h1 className="font-bold text-4xl md:text-6xl lg:text-5xl xl:text-6xl text-white">
                 Create a single link for all your social media links with{" "}
                 <span className="text-purple-500">Ownlink</span>
               </h1>
@@ -136,7 +139,7 @@ const Hero = () => {
               </p>
               <div>
                 <form action="" className="w-full mt-12">
-                  <div className="relative flex items-center px-2 p-1 rounded-full bg-gray-900  border border-gray-700 border-primary/10 shadow-md md:p-2 lg:pr-3">
+                  <div className="relative flex items-center px-2 p-1 rounded-full bg-gray-900 border border-gray-700 border-primary/10 shadow-md md:p-2 lg:pr-3">
                     <div className="pl-6 py-3">
                       <FaUser color="white" size={20} />
                     </div>
@@ -187,12 +190,11 @@ const Hero = () => {
               <img
                 src={HeroImg}
                 alt="project illustration"
-                className=" w-full h-108 rounded-2xl h-full mr-0 md:p-1 scale-95"
+                className="w-full h-108 rounded-2xl h-full mr-0 md:p-1 scale-95"
               />
             </div>
           </div>
         </motion.div>
-        {/* End of motion.div */}
       </div>
     </motion.div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,28 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ], 
-    theme: {
-      extend: {
-        boxShadow: {
-          '3xl' : '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        "3xl": "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
       },
       colors: {
-        primaryLight: '#8536D1',
-        secondaryLight: '#A855F7',
-        darker: '#090E1A',
-
+        primaryLight: "#8536D1",
+        secondaryLight: "#A855F7",
+        darker: "#090E1A",
       },
       backgroundColor: {
-        'sky-100': '#A855F7', // Define your custom color here
-        darker: '#090E1A',
+        "sky-100": "#A855F7", // Define your custom color here
+        darker: "#090E1A",
       },
 
-        
-        
-        
+      animation: {
+        flip: "flip 6s infinite steps(2, end)",
+        rotate: "rotate 3s linear infinite both",
+      },
+
+      keyframes: {
+        flip: {
+          to: {
+            transform: "rotate(360deg)",
+          },
+        },
+        rotate: {
+          to: {
+            transform: "rotate(90deg)",
+          },
+        },
       },
     },
+  },
   plugins: [],
-}
+};
